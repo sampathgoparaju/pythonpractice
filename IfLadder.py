ede540fdd3c855e5c55a5a8c9807b4a9840f065a
Ubuntu <ubuntu@ip-172-31-35-41.ap-south-1.compute.internal>
2020-10-20 11:21:44 +0000

    IfLadder

---
+++ IfLadder.py
@@ -0,0 +1,19 @@
+#IfLadder condition
+
+#!/usr/bin/python
+
+a='o'
+
+
+if a==a:
+    print 'The character of A is vowel:',a
+elif a==e:
+    print 'The character of A is vowel:',e
+elif a==i:
+    print 'The character of A is vowel:',i
+elif a==o:
+    print 'The character of A is vowel:',o
+elif a==u:
+    print 'The character of A is vowel:',u
+else:
+    print 'Good Bye'
