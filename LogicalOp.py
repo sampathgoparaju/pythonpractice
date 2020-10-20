f7e9921740612b7864ec2df94663585e5db98af7
Ubuntu <ubuntu@ip-172-31-35-41.ap-south-1.compute.internal>
2020-10-20 08:46:02 +0000

    Logical

---
+++ LogicalOp.py
@@ -0,0 +1,16 @@
+# Using logical operators
+
+#!/usr/bin/python
+
+x=10
+y=5
+
+
+if x>y and not x == 10:
+    print('Value of x is TEN')
+    print('We tried AND operator')
+
+else:
+    print('Try another operator')
+
+print('Operators program')
