0ec08cca358378acdb8930fe0ee54fc895430383
Ubuntu <ubuntu@ip-172-31-35-41.ap-south-1.compute.internal>
2020-10-20 07:15:10 +0000

    Python bare if program

---
+++ bareif.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+#bare if condition
+
+a=raw_input('Enter any number:')
+b=raw_input('Enter any number:')
+
+print ('The values of the numbers are %d and %d',a and b)
+
+if a>b
+    print ("Value of a is greater than b")
+    print ("this is bare if condition")
+
+print('First conditional operators program')
