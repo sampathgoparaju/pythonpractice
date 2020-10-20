2e7f5459534f0b55061a250e602f4c9f1863f003
Ubuntu <ubuntu@ip-172-31-35-41.ap-south-1.compute.internal>
2020-10-20 08:01:16 +0000

    IFelse program

---
+++ Ifelse.py
@@ -0,0 +1,18 @@
+#if else condition
+
+#!/usr/bin/python
+
+a=raw_input('Enter any number:')
+b=raw_input('Enter any number:')
+
+print 'The values of the numbers are %d and %d',(a,b)
+
+if a>b:
+    print ("Value of a is greater than b")
+    print ("this is if condition")
+else 
+    print ("Value of b is greater than a")
+    print ("this is if condition")
+
+print('Second condition statements program')
+
