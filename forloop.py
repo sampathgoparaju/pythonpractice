ab054780c439409efc05868af3a86d8fb2fd3259
Ubuntu <ubuntu@ip-172-31-35-41.ap-south-1.compute.internal>
2020-10-20 12:04:47 +0000

    for and while loop

---
+++ forloop.py
@@ -0,0 +1,8 @@
+#For loop
+
+#!/usr/bin/python
+
+for x in 1,1.5,"Sampath",True:
+    print 'The value of x:',x,"type is :",type(x)
+
+print 'Forloop program'
---
+++ whileloop.py
@@ -0,0 +1,17 @@
+#while loop 
+
+#!/usr/bin/python
+
+x=1
+
+while x<=10:
+    print x,
+    x+=1
+
+print '\nWhile loop Program'
+y=1
+while y!=11:
+    print '\nI am Sampath',
+    y+=1
+
+print '\nFirst time while loop program'
