d179596d542edfab3f8fe6dc121cfefb684f0b05
Ubuntu <ubuntu@ip-172-31-35-41.ap-south-1.compute.internal>
2020-10-20 07:21:57 +0000

    bareifmodified

--- bareif.py
+++ bareif.py
@@ -7,7 +7,7 @@ b=raw_input('Enter any number:')
 
 print ('The values of the numbers are %d and %d',a and b)
 
-if a>b
+if a>b:
     print ("Value of a is greater than b")
     print ("this is bare if condition")
 
