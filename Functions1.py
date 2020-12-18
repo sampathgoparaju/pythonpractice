bed1705bddac39cd2eed504cedad72d37c494ff6
root <root@ip-172-31-44-111.ap-south-1.compute.internal>
2020-12-18 02:31:57 +0000

    BuiltinFunctions

---
+++ Functions1.py
@@ -0,0 +1,17 @@
+#! /bin/bash
+##This is for builtin Functions
+
+a= input('Enter any number:')
+b= input('Enter any number:')
+c= input('Enter any number:')
+d= input('Enter any number:')
+
+print 'The absolute value is', abs(-45)
+print 'The comparision value of a and b is', cmp(a,b)
+print 'The maximum of a,b is', max(a,b)
+print 'The minimum of a,b is', min(a,b)
+print 'The power raised of c to d is', pow(c,d)
+print 'The Type specification of c is', type(c)
+print 'The addess location of value a is', id(a)
+print 'The length of the string Python is', len("Python")
+
