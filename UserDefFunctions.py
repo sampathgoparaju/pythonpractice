ef48a199425dc0e93294e15c5e31959c62ba63e3
root <root@ip-172-31-32-209.ap-south-1.compute.internal>
2020-12-18 15:33:24 +0000

    Functions

---
+++ UserDefFunctions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+#demonstrating user defined functions
+
+def functionnumbers():
+    print 'The value is 2'
+    print 'The value is 3'
+
+print 'The value is 1'
+functionnumbers()
+print 'The values is 4'
+print 'The END'
