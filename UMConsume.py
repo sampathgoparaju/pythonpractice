213ede60c16f2b04a1895e597bdb30ce401f09d5
root <root@ip-172-31-32-58.ap-south-1.compute.internal>
2020-12-21 10:59:34 +0000

    userdefined consume latest

--- UMConsume.py
+++ UMConsume.py
@@ -2,10 +2,8 @@
 
 #this is the file having statements to be executed using the user defined modules
 import UMbody as umb
-
-x=10
-y=5
-
+x=input('Enter any numer:')
+y=input('Enter any numer:')
 
 print 'Addition of x and y is', umb.add(x,y)
 
@@ -15,4 +13,5 @@ print 'Multiplication of x and y is', umb.mul(x,y)
 
 print 'Division of x and y is', umb.div(x,y)
 
+print 'the data values of a and b is', umb.a, umb.b
 print 'END'
