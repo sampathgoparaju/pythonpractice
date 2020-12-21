66ad6c3d7bbfa09ba9908ec4ad4cf7e18510c80c
root <root@ip-172-31-14-14.ap-south-1.compute.internal>
2020-12-21 08:26:10 +0000

    user body consume file

---
+++ UMConsume.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+#this is the file having statements to be executed using the user defined modules
+import UMbody as umb
+
+x=10
+y=5
+
+
+print 'Addition of x and y is', umb.add(x,y)
+
+print 'Subtraction of x and y is', umb.sub(x,y)
+
+print 'Multiplication of x and y is', umb.mul(x,y)
+
+print 'Division of x and y is', umb.div(x,y)
+
+print 'END'
