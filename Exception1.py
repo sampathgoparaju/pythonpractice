784bed361428d895c84e616995afb9a19b4f6f16
root <root@ip-172-31-10-97.ap-south-1.compute.internal>
2020-12-22 11:30:46 +0000

    Exception handling

---
+++ Exception1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+# Exception handling 
+x=input("Enter any number:")
+y=input("Enter any number:")
+
+print 'The values of x and y are', x,y
+
+res=x/y
+
+print 'The output of the division is', res
+
+
