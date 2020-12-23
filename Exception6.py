2ee6f82a43f61734d20f23da1f355dabf665108f
root <root@ip-172-31-41-55.ap-south-1.compute.internal>
2020-12-23 01:52:32 +0000

    Else and finally statements

---
+++ Exception6.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+# Exception handling 
+try:
+    x=input("Enter any number:")
+    y=input("Enter any number:")
+
+    print 'The values of x and y are', x,y
+
+    res=x/y
+
+    print 'The output of the division is', res
+    print "I have executed"
+
+except ZeroDivisionError,e:
+    print 'ZDE:Error encountered, trying to divide number by zero',e
+except NameError,e:
+    print 'NE:The given variable is not defined in program',e
+except TypeError,e:
+    print 'TE:The given variable is not compatible',e
+except Exception,e:
+    print 'Default:This is a default exception',e
+else:
+    print 'else block is executed without errors'
+finally:
+    print 'program wxecuted with or without errors'
+ 
